d59035390861b35d2caa005bd30dc926a12c1ec5
Markus <voegtle@mail.hs-ulm.de>
2015-12-08 21:37:59 +0100

    Schnitte IDrawable hinzugefügt

---
+++ f_spielprojekt/IDrawable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Stickman
+{
+    interface IDrawable
+    {
+        void Zeichne(Pen pen, Graphics g);
+    }
+}
