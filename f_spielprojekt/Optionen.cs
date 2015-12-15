fa899b3a5c3d34ac3c1830e14b4b05153fab6622
Roman Piskun <piskun@hs-ulm.de>
2015-12-15 14:06:35 +0100

    Alles aktualisiert auf einmal...

--- f_spielprojekt/Optionen.cs
+++ f_spielprojekt/Optionen.cs
@@ -1,11 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace F_Spielprojekt
 {
-    public class Optionen
+    public partial class Optionen : Form
     {
+        private int lvl;
+        public Optionen()
+        {
+            InitializeComponent();
+        }
+
+        public int Lvl
+        { get {return lvl;}}
+
+        private void rbLeicht_CheckedChanged(object sender, EventArgs e)    //Leicht mit 3 Farben
+        {
+            if (rbLeicht.Checked)
+            {
+                lvl = 3; ;
+            }
+        }
+
+        private void rbMittel_CheckedChanged(object sender, EventArgs e)    //Mittel mit 4 Farben
+        {
+            if (rbMittel.Checked)
+            {
+                lvl = 4;
+            }
+        }
+
+        private void rbSchwer_CheckedChanged(object sender, EventArgs e)    //Schwer mit 5 Farben
+        {
+            if (rbSchwer.Checked)
+            {
+                lvl = 5;
+            }
+        }
+
+        private void bSpeichern_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
