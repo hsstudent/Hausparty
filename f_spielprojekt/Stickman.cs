3112d7b5aafa58588189b05d496532b59905d467
Markus <voegtle@mail.hs-ulm.de>
2015-12-20 12:29:42 +0100

    Stickman überarbeitet, versucht bug zu beheben

--- f_spielprojekt/Stickman.cs
+++ f_spielprojekt/Stickman.cs
@@ -48,47 +48,75 @@ namespace F_Spielprojekt
 
         public void Zeichne(int schritte)       // Zeichnet die Stickmans
         {
-            schritte = (schritte % 6) + 1;
+            schritte = (schritte % 6);
 
-            if (schritte == 1)                                                           // Schritt ist für die animation zuständig (1 - 6)
+            if (schritte == 0)                                                           // Schritt ist für die animation zuständig (1 - 6)
             {
-                Point[] points = new Point[14];
-                points[0] = new Point(meinPunkt.X - 6, meinPunkt.Y + 15);     //linke Hand
-                points[1] = new Point(meinPunkt.X - 4, meinPunkt.Y + 8);      // linker Ellenbogen
-                points[2] = new Point(meinPunkt.X, meinPunkt.Y);               // Oberkörper mitte
-                points[3] = new Point(meinPunkt.X + 4, meinPunkt.Y + 8);      // rechter Ellenbogen
-                points[4] = new Point(meinPunkt.X + 9, meinPunkt.Y + 11);     // rechte Hand
-                points[5] = points[3];
-                points[6] = points[2];
-                points[7] = new Point(meinPunkt.X - 1, meinPunkt.Y + 20);      // Hüfte
-                points[8] = new Point(meinPunkt.X - 3, meinPunkt.Y + 30);      // linkes Knie
-                points[9] = new Point(meinPunkt.X - 7, meinPunkt.Y + 40);     // linker Fuß
-                points[10] = points[8];
-                points[11] = points[7];
-                points[12] = new Point(meinPunkt.X + 3, meinPunkt.Y + 30);     // rechtes Knie
-                points[13] = new Point(meinPunkt.X + 5, meinPunkt.Y + 40);    // rechter Fuß
+                Point[] points = 
+                {
+                    new Point(meinPunkt.X - 6, meinPunkt.Y + 15),     //linke Hand
+                    new Point(meinPunkt.X - 4, meinPunkt.Y + 8),      // linker Ellenbogen
+                    new Point(meinPunkt.X, meinPunkt.Y),               // Oberkörper mitte
+                    new Point(meinPunkt.X + 4, meinPunkt.Y + 8),      // rechter Ellenbogen
+                    new Point(meinPunkt.X + 9, meinPunkt.Y + 11),     // rechte Hand
+                    new Point(meinPunkt.X + 4, meinPunkt.Y + 8),
+                    new Point(meinPunkt.X, meinPunkt.Y),
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 20),      // Hüfte
+                    new Point(meinPunkt.X - 3, meinPunkt.Y + 30),      // linkes Knie
+                    new Point(meinPunkt.X - 7, meinPunkt.Y + 40),     // linker Fuß
+                    new Point(meinPunkt.X - 3, meinPunkt.Y + 30),
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 20),
+                    new Point(meinPunkt.X + 3, meinPunkt.Y + 30),     // rechtes Knie
+                    new Point(meinPunkt.X + 5, meinPunkt.Y + 40),    // rechter Fuß
+                };
                 
                 g.DrawLines(pen, points);
                 g.DrawEllipse(pen, meinPunkt.X - 2, meinPunkt.Y - 9, 9, 9); // Kopf
                 g.FillEllipse(myBrush, meinPunkt.X - 2, meinPunkt.Y - 9, 9, 9);
             }
+            else if (schritte == 1)
+            {
+                Point[] points = 
+                {
+                    new Point(meinPunkt.X - 3, meinPunkt.Y + 16),      //linke Hand
+                    new Point(meinPunkt.X - 2, meinPunkt.Y + 7),      // linker Ellenbogen
+                    new Point(meinPunkt.X, meinPunkt.Y),               // Oberkörper mitte
+                    new Point(meinPunkt.X + 4, meinPunkt.Y + 7),      // rechter Ellenbogen
+                    new Point(meinPunkt.X + 5, meinPunkt.Y + 13),     // rechte Hand
+                    new Point(meinPunkt.X + 4, meinPunkt.Y + 7),
+                    new Point(meinPunkt.X, meinPunkt.Y),
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 20),      // Hüfte
+                    new Point(meinPunkt.X - 2, meinPunkt.Y + 29),      // linkes Knie
+                    new Point(meinPunkt.X - 7, meinPunkt.Y + 38),     // linker Fuß
+                    new Point(meinPunkt.X - 2, meinPunkt.Y + 29),
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 20),
+                    new Point(meinPunkt.X + 2, meinPunkt.Y + 30),     // rechtes Knie
+                    new Point(meinPunkt.X + 3, meinPunkt.Y + 40),     // rechter Fuß
+                };
+                
+                g.DrawLines(pen, points);
+                g.DrawEllipse(pen, meinPunkt.X - 3, meinPunkt.Y - 10, 9, 9); // Kopf
+                g.FillEllipse(myBrush, meinPunkt.X - 3, meinPunkt.Y - 10, 9, 9);
+            }
             else if (schritte == 2)
             {
-                Point[] points = new Point[14];
-                points[0] = new Point(meinPunkt.X - 3, meinPunkt.Y + 16);      //linke Hand
-                points[1] = new Point(meinPunkt.X - 2, meinPunkt.Y + 7);      // linker Ellenbogen
-                points[2] = new Point(meinPunkt.X, meinPunkt.Y);               // Oberkörper mitte
-                points[3] = new Point(meinPunkt.X + 4, meinPunkt.Y + 7);      // rechter Ellenbogen
-                points[4] = new Point(meinPunkt.X + 5, meinPunkt.Y + 13);     // rechte Hand
-                points[5] = points[3];
-                points[6] = points[2];
-                points[7] = new Point(meinPunkt.X - 1, meinPunkt.Y + 20);      // Hüfte
-                points[8] = new Point(meinPunkt.X - 2, meinPunkt.Y + 29);      // linkes Knie
-                points[9] = new Point(meinPunkt.X - 7, meinPunkt.Y + 38);     // linker Fuß
-                points[10] = points[8];
-                points[11] = points[7];
-                points[12] = new Point(meinPunkt.X + 2, meinPunkt.Y + 30);     // rechtes Knie
-                points[13] = new Point(meinPunkt.X + 3, meinPunkt.Y + 40);     // rechter Fuß
+                Point[] points = 
+                {
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 17),      //linke Hand
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 7),     // linker Ellenbogen
+                    new Point(meinPunkt.X, meinPunkt.Y),               // Oberkörper mitte
+                    new Point(meinPunkt.X + 1, meinPunkt.Y + 7),     // rechter Ellenbogen
+                    new Point(meinPunkt.X + 1, meinPunkt.Y + 15),     // rechte Hand
+                    new Point(meinPunkt.X, meinPunkt.Y),
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 7),
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 20),      // Hüfte
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 29),      // linkes Knie
+                    new Point(meinPunkt.X - 6, meinPunkt.Y + 36),     // linker Fuß
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 29),
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 20),                                        
+                    new Point(meinPunkt.X + 1, meinPunkt.Y + 30),     // rechtes Knie
+                    new Point(meinPunkt.X + 1, meinPunkt.Y + 40),     // rechter Fuß
+                };
                 
                 g.DrawLines(pen, points);
                 g.DrawEllipse(pen, meinPunkt.X - 3, meinPunkt.Y - 10, 9, 9); // Kopf
@@ -96,43 +124,47 @@ namespace F_Spielprojekt
             }
             else if (schritte == 3)
             {
-                Point[] points = new Point[14];
-                points[0] = new Point(meinPunkt.X - 1, meinPunkt.Y + 17);      //linke Hand
-                points[1] = new Point(meinPunkt.X - 1, meinPunkt.Y + 7);      // linker Ellenbogen
-                points[2] = new Point(meinPunkt.X, meinPunkt.Y);               // Oberkörper mitte
-                points[3] = new Point(meinPunkt.X + 1, meinPunkt.Y + 7);      // rechter Ellenbogen
-                points[4] = new Point(meinPunkt.X + 1, meinPunkt.Y + 15);      // rechte Hand
-                points[5] = points[3];
-                points[6] = points[2];
-                points[7] = new Point(meinPunkt.X - 1, meinPunkt.Y + 20);      // Hüfte
-                points[8] = new Point(meinPunkt.X - 1, meinPunkt.Y + 29);      // linkes Knie
-                points[9] = new Point(meinPunkt.X - 6, meinPunkt.Y + 36);     // linker Fuß
-                points[10] = points[8];
-                points[11] = points[7];
-                points[12] = new Point(meinPunkt.X + 1, meinPunkt.Y + 30);     // rechtes Knie
-                points[13] = new Point(meinPunkt.X + 1, meinPunkt.Y + 40);     // rechter Fuß
+                Point[] points = 
+                {
+                    new Point(meinPunkt.X + 1, meinPunkt.Y + 17),     //linke Hand
+                    new Point(meinPunkt.X, meinPunkt.Y + 7),         // linker Ellenbogen
+                    new Point(meinPunkt.X, meinPunkt.Y),             // Oberkörper mitte
+                    new Point(meinPunkt.X, meinPunkt.Y + 7),         // rechter Ellenbogen
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 17),     // rechte Hand
+                    new Point(meinPunkt.X, meinPunkt.Y + 7),
+                    new Point(meinPunkt.X, meinPunkt.Y),
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 20),      // Hüfte
+                    new Point(meinPunkt.X, meinPunkt.Y + 28),          // linkes Knie
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 37),      // linker Fuß
+                    new Point(meinPunkt.X, meinPunkt.Y + 28),
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 20),
+                    new Point(meinPunkt.X, meinPunkt.Y + 30),         // rechtes Knie
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 40),     // rechter Fuß
+                };
                 
                 g.DrawLines(pen, points);
-                g.DrawEllipse(pen, meinPunkt.X - 3, meinPunkt.Y - 10, 9, 9); // Kopf
-                g.FillEllipse(myBrush, meinPunkt.X - 3, meinPunkt.Y - 10, 9, 9);
+                g.DrawEllipse(pen, meinPunkt.X - 2, meinPunkt.Y - 9, 9, 9); // Kopf
+                g.FillEllipse(myBrush, meinPunkt.X - 2, meinPunkt.Y - 9, 9, 9);
             }
             else if (schritte == 4)
             {
-                Point[] points = new Point[14];
-                points[0] = new Point(meinPunkt.X + 1, meinPunkt.Y + 17);     //linke Hand
-                points[1] = new Point(meinPunkt.X, meinPunkt.Y + 7);         // linker Ellenbogen
-                points[2] = new Point(meinPunkt.X, meinPunkt.Y);              // Oberkörper mitte
-                points[3] = new Point(meinPunkt.X, meinPunkt.Y + 7);         // rechter Ellenbogen
-                points[4] = new Point(meinPunkt.X - 1, meinPunkt.Y + 17);     // rechte Hand
-                points[5] = points[3];
-                points[6] = points[2];
-                points[7] = new Point(meinPunkt.X - 1, meinPunkt.Y + 20);      // Hüfte
-                points[8] = new Point(meinPunkt.X, meinPunkt.Y + 28);          // linkes Knie
-                points[9] = new Point(meinPunkt.X - 1, meinPunkt.Y + 37);      // linker Fuß
-                points[10] = points[8];
-                points[11] = points[7];
-                points[12] = new Point(meinPunkt.X, meinPunkt.Y + 30);         // rechtes Knie
-                points[13] = new Point(meinPunkt.X - 1, meinPunkt.Y + 40);     // rechter Fuß
+                Point[] points = 
+                {
+                    new Point(meinPunkt.X + 3, meinPunkt.Y + 15),      //linke Hand
+                    new Point(meinPunkt.X + 1, meinPunkt.Y + 7),      // linker Ellenbogen
+                    new Point(meinPunkt.X, meinPunkt.Y),               // Oberkörper mitte
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 7),      // rechter Ellenbogen
+                    new Point(meinPunkt.X - 3, meinPunkt.Y + 17),      // rechte Hand
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 7),
+                    new Point(meinPunkt.X, meinPunkt.Y),
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 20),      // Hüfte
+                    new Point(meinPunkt.X + 1, meinPunkt.Y + 28),      // linkes Knie
+                    new Point(meinPunkt.X + 1, meinPunkt.Y + 38),      // linker Fuß
+                    new Point(meinPunkt.X + 1, meinPunkt.Y + 28),
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 20),
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 30),     // rechtes Knie
+                    new Point(meinPunkt.X - 3, meinPunkt.Y + 40),    // rechter Fuß
+                };
                 
                 g.DrawLines(pen, points);
                 g.DrawEllipse(pen, meinPunkt.X - 2, meinPunkt.Y - 9, 9, 9); // Kopf
@@ -140,43 +172,23 @@ namespace F_Spielprojekt
             }
             else if (schritte == 5)
             {
-                Point[] points = new Point[14];
-                points[0] = new Point(meinPunkt.X + 3, meinPunkt.Y + 15);      //linke Hand
-                points[1] = new Point(meinPunkt.X + 1, meinPunkt.Y + 7);      // linker Ellenbogen
-                points[2] = new Point(meinPunkt.X, meinPunkt.Y);               // Oberkörper mitte
-                points[3] = new Point(meinPunkt.X - 1, meinPunkt.Y + 7);      // rechter Ellenbogen
-                points[4] = new Point(meinPunkt.X - 3, meinPunkt.Y + 17);      // rechte Hand
-                points[5] = points[3];
-                points[6] = points[2];
-                points[7] = new Point(meinPunkt.X - 1, meinPunkt.Y + 20);      // Hüfte
-                points[8] = new Point(meinPunkt.X + 1, meinPunkt.Y + 28);      // linkes Knie
-                points[9] = new Point(meinPunkt.X + 1, meinPunkt.Y + 38);      // linker Fuß
-                points[10] = points[8];
-                points[11] = points[7];
-                points[12] = new Point(meinPunkt.X - 1, meinPunkt.Y + 30);     // rechtes Knie
-                points[13] = new Point(meinPunkt.X - 3, meinPunkt.Y + 40);     // rechter Fuß
-                
-                g.DrawLines(pen, points);
-                g.DrawEllipse(pen, meinPunkt.X - 2, meinPunkt.Y - 9, 9, 9); // Kopf
-                g.FillEllipse(myBrush, meinPunkt.X - 2, meinPunkt.Y - 9, 9, 9);
-            }
-            else if (schritte == 6)
-            {
-                Point[] points = new Point[14];
-                points[0] = new Point(meinPunkt.X + 6, meinPunkt.Y + 13);     //linke Hand
-                points[1] = new Point(meinPunkt.X + 2, meinPunkt.Y + 7);      // linker Ellenbogen
-                points[2] = new Point(meinPunkt.X, meinPunkt.Y);               // Oberkörper mitte
-                points[3] = new Point(meinPunkt.X - 2, meinPunkt.Y + 7);      // rechter Ellenbogen
-                points[4] = new Point(meinPunkt.X - 5, meinPunkt.Y + 16);     // rechte Hand
-                points[5] = points[3];
-                points[6] = points[2];
-                points[7] = new Point(meinPunkt.X - 1, meinPunkt.Y + 20);      // Hüfte
-                points[8] = new Point(meinPunkt.X + 2, meinPunkt.Y + 28);      // linkes Knie
-                points[9] = new Point(meinPunkt.X + 5, meinPunkt.Y + 38);     // linker Fuß
-                points[10] = points[8];
-                points[11] = points[7];
-                points[12] = new Point(meinPunkt.X - 2, meinPunkt.Y + 30);     // rechtes Knie
-                points[13] = new Point(meinPunkt.X - 5, meinPunkt.Y + 40);    // rechter Fuß
+                Point[] points = 
+                {
+                    new Point(meinPunkt.X + 6, meinPunkt.Y + 13),     //linke Hand
+                    new Point(meinPunkt.X + 2, meinPunkt.Y + 7),      // linker Ellenbogen
+                    new Point(meinPunkt.X, meinPunkt.Y),              // Oberkörper mitte
+                    new Point(meinPunkt.X - 2, meinPunkt.Y + 7),      // rechter Ellenbogen
+                    new Point(meinPunkt.X - 5, meinPunkt.Y + 16),     // rechte Hand
+                    new Point(meinPunkt.X - 2, meinPunkt.Y + 7),
+                    new Point(meinPunkt.X, meinPunkt.Y),
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 20),      // Hüfte
+                    new Point(meinPunkt.X + 2, meinPunkt.Y + 28),      // linkes Knie
+                    new Point(meinPunkt.X + 5, meinPunkt.Y + 38),     // linker Fuß
+                    new Point(meinPunkt.X + 2, meinPunkt.Y + 28),
+                    new Point(meinPunkt.X - 1, meinPunkt.Y + 20),
+                    new Point(meinPunkt.X - 2, meinPunkt.Y + 30),     // rechtes Knie
+                    new Point(meinPunkt.X - 5, meinPunkt.Y + 40),    // rechter Fuß
+                };
                 
                 g.DrawLines(pen, points);
                 g.DrawEllipse(pen, meinPunkt.X - 2, meinPunkt.Y - 9, 9, 9); // Kopf
